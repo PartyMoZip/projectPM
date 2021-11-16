0587d680c70cf566b80ae108f1e480b457801581
SIAN <chicori3@naver.com>
2021-11-17 01:36:31 +0900

    [ADD] 메인페이지 내 파티 목록 조회 mapper, service 추가

--- src/main/java/com/pm/myapp/mapper/MainMapper.java
+++ src/main/java/com/pm/myapp/mapper/MainMapper.java
@@ -1,12 +1,14 @@
-package com.pm.myapp.service.main;
+package com.pm.myapp.mapper;
+
 
 import com.pm.myapp.domain.MyPartyVO;
 
 import java.util.List;
 
-public interface MainPageService {
+// 메인페이지 매퍼
+public interface MainMapper {
 
     // 내 파티 목록 조회
     public abstract List<MyPartyVO> getMyPartyList(String email);
 
-} //end interface
+} // end interface
--- src/main/java/com/pm/myapp/mapper/xxxMapper.java
+++
@@ -1,9 +0,0 @@
-package com.pm.myapp.mapper;
-
-
-public interface xxxMapper {
-
-	public abstract void xxx();
-	
-	
-} // end interface
