8e518dfa269d2d77880dba0874f7dd7815a4a1d5
SIAN <chicori3@naver.com>
2021-11-17 21:18:45 +0900

    [FIX] 홈컨트롤러 수정, Mapper 수정

--- src/main/java/com/pm/myapp/controller/HomeController.java
+++ src/main/java/com/pm/myapp/controller/HomeController.java
@@ -1,12 +1,21 @@
 package com.pm.myapp.controller;
 
+import com.pm.myapp.controller.join.LoginController;
+import com.pm.myapp.domain.PartyVO;
+import com.pm.myapp.domain.UserDTO;
+import com.pm.myapp.service.main.MainPageService;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
 import java.util.Locale;
 
 @Log4j2
@@ -14,10 +23,25 @@ import java.util.Locale;
 @Controller
 public class HomeController {
 
+    @Setter(onMethod_ = {@Autowired})
+    private MainPageService service;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String home(Locale locale, Model model) {
+    public String home(Locale locale, Model model, HttpServletRequest req) {
         log.debug("home() invoked.");
+
+        HttpSession session = req.getSession();
+        log.info("\t+ session : {}", session);
+        if (session.getAttribute(LoginController.authKey) != null) {
+
+            UserDTO dto = (UserDTO) session.getAttribute(LoginController.authKey);
+
+            List<PartyVO> list = this.service.getMyPartyList(dto.getEmail());
+
+            model.addAttribute("list", list);
+        } else {
+            // List<MyPartyVO>
+        }
 
         return "index";
     }
--- src/main/java/com/pm/myapp/interceptor/AuthInterceptor.java
+++ src/main/java/com/pm/myapp/interceptor/AuthInterceptor.java
@@ -21,10 +21,10 @@ import javax.servlet.http.HttpSession;
 @Component
 public class AuthInterceptor implements HandlerInterceptor {
 
-    public static final String requestURIKey="__REQUEST_URI__";
-    public static final String queryStringKey="__QUERYSTRING__";
+    public static final String requestURIKey = "__REQUEST_URI__";
+    public static final String queryStringKey = "__QUERYSTRING__";
 
-    @Setter(onMethod_= {@Autowired})
+    @Setter(onMethod_ = {@Autowired})
     private LoginService loginService;
 
 
@@ -38,15 +38,15 @@ public class AuthInterceptor implements HandlerInterceptor {
         //--------------------------------//
         // 1. 현재 요청을 보낸 브라우저를 위한 Session Scope에 인증정보가 있는지 확인
         //--------------------------------//
-        HttpSession session=req.getSession();
-        UserDTO user=(UserDTO) session.getAttribute(LoginController.authKey);
+        HttpSession session = req.getSession();
+        UserDTO user = (UserDTO) session.getAttribute(LoginController.authKey);
         log.info("\t+ user: {}", user);
 
-        if(user == null) { // 아직 인증되지 않은 상태이다...
+        if (user == null) { // 아직 인증되지 않은 상태이다...
 
             // 1. 원래의 Original Request URI + 모든 전송파라미터까지를 Session Scope 에 저장.
-            String originReqURI=req.getRequestURI();
-            String originQueryString=req.getQueryString();
+            String originReqURI = req.getRequestURI();
+            String originQueryString = req.getQueryString();
 
             session.setAttribute(AuthInterceptor.requestURIKey, originReqURI);
             session.setAttribute(AuthInterceptor.queryStringKey, originQueryString);
@@ -54,7 +54,6 @@ public class AuthInterceptor implements HandlerInterceptor {
             // 2. 현재의 요청이 회원만이 보낼 수 있는 요청이라면(로그인 된 이후 사용할수 있는 메뉴라면) --> 로그인 창으로 이동
             res.sendRedirect("/login/loginPage");
         } // if
-
 
         // 가로챈 요청을, 이 인터셉터 뒤에, 컨트롤러가 있으면 --> 컨트롤러에게 요청을 넘겨주겠다.
         // 가로챈 요청을, 이 인터셉터 뒤에, 체인이 있으면 -----> 체인에게 요청을 넘겨주겠다.
