c3f3fd1d6b9c872c50aed73aa56ddaafc90f8bde
SIAN <chicori3@naver.com>
2021-11-24 20:52:57 +0900

    [MOD] 헤더, 메인, 검색 페이지 스타일 / JS 수정

--- src/main/webapp/WEB-INF/views/join/join.jsp
+++ src/main/webapp/WEB-INF/views/join/join.jsp
@@ -8,7 +8,7 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Join</title>
+    <title>파티모집 - 로그인</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="icon" href="${pageContext.request.contextPath}/resources/images/favicon.ico"/>
 
--- src/main/webapp/resources/js/search.js
+++ src/main/webapp/resources/js/search.js
@@ -6,12 +6,42 @@ const hobbySearchBar = document.querySelector(".select-hobby");
 const localSelectInput = document.querySelector(".select-local");
 const searchInput = document.querySelector(".input-search");
 const selectForm = document.querySelector(".select-form");
+const headerForm = document.querySelector(".header-form");
+const headerSearch = document.querySelector(".header-search");
 const paginationForm = document.querySelector("#paginationForm");
 
+const checkSpell = (word) => {
+    const regExp = /[!?@#$%^&*():;+-=~{}<>\_\[\]\|\\\"\'\,\.\/\`\₩]/g;
+    const regExp2 = /[ㄱ-ㅎㅏ-ㅣ]/g;
+    let result = "true";
+
+    if (regExp.test(word) || regExp2.test(word)) {
+        result = false;
+    }
+
+    return result;
+}
 
 // 취미 메뉴 검색 창 클릭 이벤트 막기
 const handlePrevent = (e) => {
     e.stopPropagation();
+}
+
+// 헤더 검색창 이벤트
+const handleHeaderSubmit = (e) => {
+    e.preventDefault();
+
+    if (!checkSpell(headerSearch.value)) {
+        Swal.fire(
+            "입력 오류",
+            "특수문자나 단순 자모음은 검색할 수 없습니다.",
+            "warning"
+        )
+
+    } else {
+        headerForm.submit();
+    }
+
 }
 
 // 취미, 지역 카테고리 선택
@@ -37,7 +67,8 @@ const handleClickCategory = (e) => {
     selectForm.submit();
 }
 
-
+headerForm.addEventListener('submit', handleHeaderSubmit);
+// headerSearch.addEventListener('submit', handleHeaderSubmit);
 hobbySearchBar.addEventListener('click', handlePrevent);
 hobbyMenu.addEventListener("click", handleClickCategory);
 localMenu.addEventListener("click", handleClickCategory);
