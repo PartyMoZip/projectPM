5016a6d4fdf77dade2f603b7dbf5e2d89e4b8657
SIAN <chicori3@naver.com>
2021-11-27 16:31:28 +0900

    [FIX] PartyVO 수정으로 인한 ORA-00937 해결

--- src/main/java/com/pm/myapp/domain/PartyVO.java
+++ src/main/java/com/pm/myapp/domain/PartyVO.java
@@ -1,22 +1,23 @@
 package com.pm.myapp.domain;
 
+import lombok.Data;
+
 import java.sql.Date;
-import lombok.Value;
 
 
-@Value
+@Data
 public class PartyVO {
 
-	private Integer count;
-	private Integer partyCode;
-	private String partyName;
-	private Integer partyScore;
-	private Date createDate;
-	private String logoPic;
-	private String coverPic;
-	private String partyProfile;
-	private Integer partyBanned;
-	private String localName;
-	private String hobbyName;
+    private Integer count;
+    private Integer partyCode;
+    private String partyName;
+    private Integer partyScore;
+    private Date createDate;
+    private String logoPic;
+    private String coverPic;
+    private String partyProfile;
+    private Integer partyBanned;
+    private String localName;
+    private String hobbyName;
 
 } // end class
