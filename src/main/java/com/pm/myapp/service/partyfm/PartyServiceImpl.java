e817f9a6577baf9835547947e1ce50bde1685ed7
SIAN <chicori3@naver.com>
2021-12-02 02:21:04 +0900

    [FIX] 파티 생성 매퍼, 서비스 수정

--- src/main/java/com/pm/myapp/service/partyfm/PartyServiceImpl.java
+++ src/main/java/com/pm/myapp/service/partyfm/PartyServiceImpl.java
@@ -1,248 +1,240 @@
 package com.pm.myapp.service.partyfm;
 
-import java.util.List;
-import java.util.Map;
-
+import com.pm.myapp.domain.*;
+import com.pm.myapp.mapper.PartyMapper;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.pm.myapp.domain.Criteria;
-import com.pm.myapp.domain.PartyDTO;
-import com.pm.myapp.domain.PartyMemberCheckVO;
-import com.pm.myapp.domain.PartyUserVO;
-import com.pm.myapp.domain.PartyVO;
-import com.pm.myapp.domain.UserDTO;
-import com.pm.myapp.mapper.PartyMapper;
-
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.extern.log4j.Log4j2;
+import java.util.List;
+import java.util.Map;
 
 @Log4j2
 @NoArgsConstructor
 
 @Service
-public class PartyServiceImpl implements PartyService,InitializingBean, DisposableBean {
-	
-	@Setter(onMethod_= {@Autowired})
-	private PartyMapper mapper;
-	
-	//========================================================
-	
-	@Override
-	public void destroy() throws Exception {
-		log.debug("destroy() invoked.");
-		
-	} // destroy
+public class PartyServiceImpl implements PartyService, InitializingBean, DisposableBean {
 
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		log.debug("afterPropertiesSet() invoked.");
-		
-		assert this.mapper!=null;
-		log.info("\t + mapper : " + this.mapper);
-		
-	} // afterPropertiesSet
-	
-	//===========================================================================
-	
-	
-	@Override
-	public List<PartyMemberCheckVO> checkPartyMember(String email) {
-		log.debug("checkPartyMember({}) invoked.",email);
-		
-		List<PartyMemberCheckVO> checkList = this.mapper.checkIt(email);
-		
-		return checkList;
-	} // 
-	
-	@Override
-	public PartyVO getParty(Integer partyCode) {
-		log.debug("getParty({}) invoked.",partyCode);
-		
-		PartyVO party = this.mapper.getInfo(partyCode);
+    @Setter(onMethod_ = {@Autowired})
+    private PartyMapper mapper;
 
-		log.info("\t + party : {}", party);
-		
-		return party;
-		
-	} // getParty
-	
-	@Override
-	public boolean doJoin(String email, Integer partyCode) {
-		log.debug("doJoin({}, {}) invoked.",email, partyCode);
-		
-		int affectedLine = this.mapper.makeJoin(email, partyCode);
-		
-		return (affectedLine==1);
-	
-	} // doJoin
-	
-	@Override
-	public boolean undoJoin(String email, Integer partyCode) {
-		log.debug("undoJoin({}, {}) invoked.",email, partyCode);
-		
-		int affectedLine = this.mapper.deleteJoin(email, partyCode);
-		
-		return (affectedLine==1);
-		
-	} // undoJoin
+    //========================================================
 
-	@Override
-	public boolean editInfo(Map<String, Object> partyInfo) {
-		log.debug("editPartyProfile({}) invoked.",partyInfo);
+    @Override
+    public void destroy() throws Exception {
+        log.debug("destroy() invoked.");
 
-		int affectedLine = this.mapper.modifyInfo(partyInfo);
+    } // destroy
 
-		log.info("\t + affectedLine : {}",affectedLine);
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        log.debug("afterPropertiesSet() invoked.");
 
-		return (affectedLine==1);
+        assert this.mapper != null;
+        log.info("\t + mapper : " + this.mapper);
 
-	} // editPartyProfile
+    } // afterPropertiesSet
+
+    //===========================================================================
 
 
-	@Override
-	public boolean breakParty(Integer partyCode) {
-		log.debug("breakParty({}) invoked.",partyCode);
-		
-		int affectedLine = this.mapper.makeDesPartyReq(partyCode);
-		
-		log.info("\t + affectedLine : {}",affectedLine);
-		
-		return (affectedLine==1);
-		
-	} // breakParty
+    @Override
+    public List<PartyMemberCheckVO> checkPartyMember(String email) {
+        log.debug("checkPartyMember({}) invoked.", email);
 
-	@Override
-	public boolean editPL(Integer authCode, String email, Integer partyCode) {
-		log.debug("editPL({}, {}) invoked.",email,partyCode);
-		
-		int affectedLine = this.mapper.delegatePL(authCode, email, partyCode);
-		
-		log.info("\t + affectedLine : {}",affectedLine);
-		
-		return (affectedLine==1);
-	} // editPL
+        List<PartyMemberCheckVO> checkList = this.mapper.checkIt(email);
 
-	@Override
-	public boolean acceptJoin(String email, Integer partyCode) {
-		log.debug("acceptJoin({}, {}) invoked.",email, partyCode);
-		
-		int affectedLine = this.mapper.upgradeJoin(email, partyCode);
-		
-		log.info("\t + affectedLine : {}",affectedLine);
-		
-		return (affectedLine==1);
-		
-	} // acceptJoin
+        return checkList;
+    } //
 
-	@Override
-	public boolean rejectJoin(String email, Integer partyCode) {
-		log.debug("rejectJoin({}, {}) invoked.",email, partyCode);
-		
-		int affectedLine = this.mapper.deleteJoin(email, partyCode);
-		
-		log.info("\t + affectedLine : {}",affectedLine);
-		
-		return (affectedLine==1);
-		
-	} // rejectJoin
+    @Override
+    public PartyVO getParty(Integer partyCode) {
+        log.debug("getParty({}) invoked.", partyCode);
 
-	@Override
-	public List<PartyUserVO> showMember(Integer partyCode, Criteria cri) {
-		log.debug("showMember({},{}) invoked.", partyCode,cri);
-		
-		List<PartyUserVO> user = this.mapper.getMember(partyCode,cri);
-		
-		log.info("\t + user : {}",user);
-		
-		return user;
-		
-	} // showMember
+        PartyVO party = this.mapper.getInfo(partyCode);
 
-	@Override
-	public Integer getTotalMember(Integer partyCode) {
-		log.debug("getTotalMember({}) invoked.", partyCode);
-		
-		int resultNum = this.mapper.getPartyMN(partyCode);
-		log.info("\t+ resultNum : {}",resultNum);
-		
-		return resultNum;
-	} // getTotalMember
-	
-	@Override
-	public boolean kickMember(String email, Integer partyCode) {
-		log.debug("kickMember({}, {}) invoked.",email, partyCode);
-		
-		int affectedLine = this.mapper.deleteJoin(email, partyCode);
-		
-		log.info("\t + affectedLine : {}",affectedLine);
-		
-		return (affectedLine==1);
-		
-	} // kickMember
+        log.info("\t + party : {}", party);
 
-	@Override
-	public List<UserDTO> getJoinMakingList(Integer partyCode, Criteria cri) {
-		log.debug("getJoinMakingList({}, {}) invoked.",partyCode, cri);
-		
-		List<UserDTO> dto = this.mapper.getMakingList(partyCode, cri);
-		log.info("\t+ dto : ", dto.toString());
-		
-		return dto;
-		
-	} // getJoinMakingList
+        return party;
 
-	@Override
-	public Integer getTotalJoinMakeMember(Integer partyCode) {
-		log.debug("getTotalJoinMakeMember({}) invoked.", partyCode);
-		
-		Integer memberlist = this.mapper.getTotalMakingList(partyCode);
-		log.info("\t+ memberlist : ", memberlist);
-		
-		return memberlist;
-		
-	} // getTotalJoinMakeMember
+    } // getParty
 
-	@Override
-	public boolean createNewParty(PartyDTO pdto, String email) {
-		log.debug("createNewParty({}, {}) invoked.", pdto, email);
-		
-		Integer createresult = this.mapper.makeNewParty(pdto);
-		log.info("\t+ createresult : ", createresult);
+    @Override
+    public boolean doJoin(String email, Integer partyCode) {
+        log.debug("doJoin({}, {}) invoked.", email, partyCode);
 
-		Integer maxPartyCode = this.mapper.maxPartyCode();
-		log.info("\t+ maxPartyCode : ", maxPartyCode);
-		
-		Integer settingresult = this.mapper.makeLeader(maxPartyCode, email);
-		log.info("\t+ settingresult : ", settingresult);
-		
-		return (createresult + settingresult == 2);
-		
-	} // createNewParty
+        int affectedLine = this.mapper.makeJoin(email, partyCode);
 
-	@Override
-	public boolean checkPartyname(String partyName) {
-		log.debug("checkPartyname({}) invoked.", partyName);
-		
-		Integer check = this.mapper.checkName(partyName);
-		log.info("\t+ check : {}", check);
-		
-		return (check==0);
-	} // checkPartyname
+        return (affectedLine == 1);
 
-	@Override
-	public Integer getMaxPartyCode() {
-		log.debug("getMaxPartyCode() invoked.");
-		
-		Integer maxPartyCode = this.mapper.maxPartyCode();
-		log.info("\t+ maxPartyCode : ", maxPartyCode);
-		
-		return maxPartyCode;
-	} // getMaxPartyCode
+    } // doJoin
 
-	
+    @Override
+    public boolean undoJoin(String email, Integer partyCode) {
+        log.debug("undoJoin({}, {}) invoked.", email, partyCode);
+
+        int affectedLine = this.mapper.deleteJoin(email, partyCode);
+
+        return (affectedLine == 1);
+
+    } // undoJoin
+
+    @Override
+    public boolean editInfo(Map<String, Object> partyInfo) {
+        log.debug("editPartyProfile({}) invoked.", partyInfo);
+
+        int affectedLine = this.mapper.modifyInfo(partyInfo);
+
+        log.info("\t + affectedLine : {}", affectedLine);
+
+        return (affectedLine == 1);
+
+    } // editPartyProfile
+
+
+    @Override
+    public boolean breakParty(Integer partyCode) {
+        log.debug("breakParty({}) invoked.", partyCode);
+
+        int affectedLine = this.mapper.makeDesPartyReq(partyCode);
+
+        log.info("\t + affectedLine : {}", affectedLine);
+
+        return (affectedLine == 1);
+
+    } // breakParty
+
+    @Override
+    public boolean editPL(Integer authCode, String email, Integer partyCode) {
+        log.debug("editPL({}, {}) invoked.", email, partyCode);
+
+        int affectedLine = this.mapper.delegatePL(authCode, email, partyCode);
+
+        log.info("\t + affectedLine : {}", affectedLine);
+
+        return (affectedLine == 1);
+    } // editPL
+
+    @Override
+    public boolean acceptJoin(String email, Integer partyCode) {
+        log.debug("acceptJoin({}, {}) invoked.", email, partyCode);
+
+        int affectedLine = this.mapper.upgradeJoin(email, partyCode);
+
+        log.info("\t + affectedLine : {}", affectedLine);
+
+        return (affectedLine == 1);
+
+    } // acceptJoin
+
+    @Override
+    public boolean rejectJoin(String email, Integer partyCode) {
+        log.debug("rejectJoin({}, {}) invoked.", email, partyCode);
+
+        int affectedLine = this.mapper.deleteJoin(email, partyCode);
+
+        log.info("\t + affectedLine : {}", affectedLine);
+
+        return (affectedLine == 1);
+
+    } // rejectJoin
+
+    @Override
+    public List<PartyUserVO> showMember(Integer partyCode, Criteria cri) {
+        log.debug("showMember({},{}) invoked.", partyCode, cri);
+
+        List<PartyUserVO> user = this.mapper.getMember(partyCode, cri);
+
+        log.info("\t + user : {}", user);
+
+        return user;
+
+    } // showMember
+
+    @Override
+    public Integer getTotalMember(Integer partyCode) {
+        log.debug("getTotalMember({}) invoked.", partyCode);
+
+        int resultNum = this.mapper.getPartyMN(partyCode);
+        log.info("\t+ resultNum : {}", resultNum);
+
+        return resultNum;
+    } // getTotalMember
+
+    @Override
+    public boolean kickMember(String email, Integer partyCode) {
+        log.debug("kickMember({}, {}) invoked.", email, partyCode);
+
+        int affectedLine = this.mapper.deleteJoin(email, partyCode);
+
+        log.info("\t + affectedLine : {}", affectedLine);
+
+        return (affectedLine == 1);
+
+    } // kickMember
+
+    @Override
+    public List<UserDTO> getJoinMakingList(Integer partyCode, Criteria cri) {
+        log.debug("getJoinMakingList({}, {}) invoked.", partyCode, cri);
+
+        List<UserDTO> dto = this.mapper.getMakingList(partyCode, cri);
+        log.info("\t+ dto : ", dto.toString());
+
+        return dto;
+
+    } // getJoinMakingList
+
+    @Override
+    public Integer getTotalJoinMakeMember(Integer partyCode) {
+        log.debug("getTotalJoinMakeMember({}) invoked.", partyCode);
+
+        Integer memberlist = this.mapper.getTotalMakingList(partyCode);
+        log.info("\t+ memberlist : ", memberlist);
+
+        return memberlist;
+
+    } // getTotalJoinMakeMember
+
+    @Override
+    public boolean createNewParty(PartyDTO pdto, String email) {
+        log.debug("createNewParty({}, {}) invoked.", pdto, email);
+
+        Integer createresult = this.mapper.makeNewParty(pdto);
+        log.info("\t+ createresult : ", createresult);
+
+        Integer maxPartyCode = this.mapper.maxPartyCode();
+        log.info("\t+ maxPartyCode : ", maxPartyCode);
+
+        Integer settingresult = this.mapper.makeLeader(maxPartyCode - 1, email);
+        log.info("\t+ settingresult : ", settingresult);
+
+        return (createresult + settingresult == 2);
+
+    } // createNewParty
+
+    @Override
+    public boolean checkPartyname(String partyName) {
+        log.debug("checkPartyname({}) invoked.", partyName);
+
+        Integer check = this.mapper.checkName(partyName);
+        log.info("\t+ check : {}", check);
+
+        return (check == 0);
+    } // checkPartyname
+
+    @Override
+    public Integer getMaxPartyCode() {
+        log.debug("getMaxPartyCode() invoked.");
+
+        Integer maxPartyCode = this.mapper.maxPartyCode();
+        log.info("\t+ maxPartyCode : ", maxPartyCode);
+
+        return maxPartyCode;
+    } // getMaxPartyCode
+
 
 } // end class
--- src/test/java/com/pm/myapp/mapper/PartyMapperTests.java
+++ src/test/java/com/pm/myapp/mapper/PartyMapperTests.java
@@ -1,9 +1,9 @@
 package com.pm.myapp.mapper;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import com.pm.myapp.domain.*;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,15 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.pm.myapp.domain.Criteria;
-import com.pm.myapp.domain.PartyDTO;
-import com.pm.myapp.domain.PartyMemberCheckVO;
-import com.pm.myapp.domain.PartyUserVO;
-import com.pm.myapp.domain.PartyVO;
-
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.extern.log4j.Log4j2;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /*
 	PartyMapper 테스트 모두 통과
@@ -36,146 +30,147 @@ import lombok.extern.log4j.Log4j2;
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 public class PartyMapperTests {
 
-	@Setter(onMethod_= {@Autowired})
-	private PartyMapper mapper;
+    @Setter(onMethod_ = {@Autowired})
+    private PartyMapper mapper;
 
-	@Before
-	public void setup() {
-		log.debug("setup() invoked.");
-		
-		assert this.mapper != null;
-		log.info("\t + mapper : {}", this.mapper);
-		log.info("\t + type : {}", this.mapper.getClass().getName());
-	} // setup
-	
-	@Test
-	public void testCheckIt() { // 성공
-		log.debug("testCheckIt() invoked.");
-		
-		String email = "test5@test.com";
-		List<PartyMemberCheckVO> result = this.mapper.checkIt(email);
-		log.info("\t+ result : {}",result);
+    @Before
+    public void setup() {
+        log.debug("setup() invoked.");
 
-	} // testCheckIt
-	
-	
-	@Test
-	public void testGetInfo() { // 성공
-		log.debug("testGetInfo() invoked.");
-		
-		int partycode = 1;
-		PartyVO result = this.mapper.getInfo(partycode);
-		log.info("\t+ result : {}",result);
+        assert this.mapper != null;
+        log.info("\t + mapper : {}", this.mapper);
+        log.info("\t + type : {}", this.mapper.getClass().getName());
+    } // setup
 
-	} // testGetInfo
-	
-	@Test
-	public void testMakeJoin() { // 성공
-		log.debug("testMakeJoin() invoked.");
-		
-		String email = "test1@test.com";
-		int partyCode = 1;
-		int result = this.mapper.makeJoin(email,partyCode);
-		log.info("\t+ result : {}",result);
+    @Test
+    public void testCheckIt() { // 성공
+        log.debug("testCheckIt() invoked.");
 
-	} // testMakeJoin
-	
-	@Test
-	public void testDeleteJoin() { // 성공
-		log.debug("testDeleteJoin() invoked.");
-		
-		String email = "test1@test.com";
-		int partyCode = 1;
-		int result = this.mapper.deleteJoin(email,partyCode);
-		log.info("\t+ result : {}",result);
+        String email = "test5@test.com";
+        List<PartyMemberCheckVO> result = this.mapper.checkIt(email);
+        log.info("\t+ result : {}", result);
 
-	} // testDeleteJoin
-	
-	@Test
-	public void testModifyInfo() { // 성공
-		log.debug("testModifyInfo() invoked.");
+    } // testCheckIt
 
-		Map<String, Object> partyInfo = new HashMap<>();
-		partyInfo.put("partyName", "테스트");
-		partyInfo.put("partyProfile", "테스트");
-		partyInfo.put("fileLocation", "");
-		partyInfo.put("partyCode", 1);
-		
-		int result = this.mapper.modifyInfo(partyInfo);
-		log.info("\t+ result : {}",result);
 
-	} // testModifyInfo
-	
-	@Test
-	public void testMakeDesPartyReq() { // 성공
-		log.debug("testMakeDesPartyReq() invoked.");
-		
-		int partyCode = 10;
-		int result = this.mapper.makeDesPartyReq(partyCode);
-		log.info("\t+ result : {}",result);
+    @Test
+    public void testGetInfo() { // 성공
+        log.debug("testGetInfo() invoked.");
 
-	} // testModifyInfo
-	
-	@Test
-	public void testDelegatePL() { // 성공
-		log.debug("testDelegatePL() invoked.");
-		
-		int authCode = 2;
-		String email = "test1@test.com";
-		int partyCode = 10;
-		int result = this.mapper.delegatePL(authCode,email,partyCode);
-		log.info("\t+ result : {}",result);
+        int partycode = 1;
+        PartyVO result = this.mapper.getInfo(partycode);
+        log.info("\t+ result : {}", result);
 
-	} // testDelegatePL
-	
-	@Test
-	public void testUpgradeJoin() { // 성공
-		log.debug("testUpgradeJoin() invoked.");
-		
-		String email = "test1@test.com";
-		int partyCode = 10;
-		int result = this.mapper.upgradeJoin(email,partyCode);
-		log.info("\t+ result : {}",result);
+    } // testGetInfo
 
-	} // testUpgradeJoin
-	
-	@Test
-	public void testGetMember() { // 성공
-		log.debug("testGetMember() invoked.");
-		
-		Criteria cri = new Criteria();
-		
-		int partyCode = 14;
-		List<PartyUserVO> list = this.mapper.getMember(partyCode, cri);
-		log.info("\t+ list : {}",list);
+    @Test
+    public void testMakeJoin() { // 성공
+        log.debug("testMakeJoin() invoked.");
 
-	} // testGetMember
-	
-	@Test
-	public void testCreateNewParty() {
-		PartyDTO pdto = new PartyDTO();
-		pdto.setPartyName("test클럽");
-		pdto.setPartyProfile("여기는 테스트클럽");
-		pdto.setHobbyCode(1);
-		pdto.setLocalCode(2);
-		
-		String email = "thehappypool@naver.com";
-		
-		log.debug("testCreateNewParty({}, {}) invoked.", pdto, email);
-		
-		Integer createresult = this.mapper.makeNewParty(pdto);
-		log.info("\t+ createresult : ", createresult);
+        String email = "test1@test.com";
+        int partyCode = 1;
+        int result = this.mapper.makeJoin(email, partyCode);
+        log.info("\t+ result : {}", result);
 
-		Integer maxPartyCode = this.mapper.maxPartyCode();
-		log.info("\t+ maxPartyCode : ", maxPartyCode);
-		
-		Integer settingresult = this.mapper.makeLeader(maxPartyCode, email);
-		log.info("\t+ settingresult : ", settingresult);
-	} // 
-	
-	@After
-	public void tearDown() {
-		log.debug("tearDown() invoked.");
-	} // tearDown
-	
+    } // testMakeJoin
+
+    @Test
+    public void testDeleteJoin() { // 성공
+        log.debug("testDeleteJoin() invoked.");
+
+        String email = "test1@test.com";
+        int partyCode = 1;
+        int result = this.mapper.deleteJoin(email, partyCode);
+        log.info("\t+ result : {}", result);
+
+    } // testDeleteJoin
+
+    @Test
+    public void testModifyInfo() { // 성공
+        log.debug("testModifyInfo() invoked.");
+
+        Map<String, Object> partyInfo = new HashMap<>();
+        partyInfo.put("partyName", "테스트");
+        partyInfo.put("partyProfile", "테스트");
+        partyInfo.put("fileLocation", "");
+        partyInfo.put("partyCode", 1);
+
+        int result = this.mapper.modifyInfo(partyInfo);
+        log.info("\t+ result : {}", result);
+
+    } // testModifyInfo
+
+    @Test
+    public void testMakeDesPartyReq() { // 성공
+        log.debug("testMakeDesPartyReq() invoked.");
+
+        int partyCode = 10;
+        int result = this.mapper.makeDesPartyReq(partyCode);
+        log.info("\t+ result : {}", result);
+
+    } // testModifyInfo
+
+    @Test
+    public void testDelegatePL() { // 성공
+        log.debug("testDelegatePL() invoked.");
+
+        int authCode = 2;
+        String email = "test1@test.com";
+        int partyCode = 10;
+        int result = this.mapper.delegatePL(authCode, email, partyCode);
+        log.info("\t+ result : {}", result);
+
+    } // testDelegatePL
+
+    @Test
+    public void testUpgradeJoin() { // 성공
+        log.debug("testUpgradeJoin() invoked.");
+
+        String email = "test1@test.com";
+        int partyCode = 10;
+        int result = this.mapper.upgradeJoin(email, partyCode);
+        log.info("\t+ result : {}", result);
+
+    } // testUpgradeJoin
+
+    @Test
+    public void testGetMember() { // 성공
+        log.debug("testGetMember() invoked.");
+
+        Criteria cri = new Criteria();
+
+        int partyCode = 14;
+        List<PartyUserVO> list = this.mapper.getMember(partyCode, cri);
+        log.info("\t+ list : {}", list);
+
+    } // testGetMember
+
+    @Test
+    public void testCreateNewParty() {
+        PartyDTO pdto = new PartyDTO();
+        pdto.setPartyName("test777");
+        pdto.setPartyProfile("여기는 테스트클럽");
+        pdto.setHobbyCode(1);
+        pdto.setLocalCode(2);
+        pdto.setFileLocation("test");
+
+        String email = "thehappypool@naver.com";
+
+        log.debug("testCreateNewParty({}, {}) invoked.", pdto, email);
+
+        Integer createresult = this.mapper.makeNewParty(pdto);
+        log.info("\t+ createresult : ", createresult);
+
+        Integer maxPartyCode = this.mapper.maxPartyCode();
+        log.info("\t+ maxPartyCode : ", maxPartyCode);
+
+        Integer settingresult = this.mapper.makeLeader(maxPartyCode - 1, email);
+        log.info("\t+ settingresult : ", settingresult);
+    } //
+
+    @After
+    public void tearDown() {
+        log.debug("tearDown() invoked.");
+    } // tearDown
+
 } // end class
